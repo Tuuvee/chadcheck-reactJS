375c9f7bf66ff884867d7b1fee293457661caaa4
Tuuvee <tuukka.tahtinen@hotmail.fi>
2021-12-06 12:02:12 +0200

    more memes

--- src/Components/Archives.js
+++ src/Components/Archives.js
@@ -12,43 +12,48 @@ function Archives() {
 	<div className="columnArchives">
 	</div>
 		<div className="column">
-			<img src="https://cdn.discordapp.com/attachments/501074912279003166/719562454748889128/IMG_20200608_174348.png" alt="Meme"/> 
-			<img src="https://cdn.discordapp.com/attachments/501074912279003166/736899601214210098/9a1j7qu7s4d51.png" alt="Meme"/> 
-			<img src="https://cdn.discordapp.com/attachments/501074912279003166/743147194105724998/j9yhxqbnkkg51.png" alt="Meme"/> 
-			<img src="https://cdn.discordapp.com/attachments/501074912279003166/773706616658067487/Screenshot_20201105-021130_TikTok.jpg" alt="Meme"/> 
-			<img src="https://cdn.discordapp.com/attachments/501074912279003166/769637674344251403/ujw9osgty1v51.png" alt="Meme"/> 
-			<img src="https://cdn.discordapp.com/attachments/501074912279003166/767152815914352670/alxtr1hi2pt51.png" alt="Meme"/> 
-			<img src="https://cdn.discordapp.com/attachments/501074912279003166/765397628228730890/wnbwrtemjps51.png" alt="Meme"/> 
-			<img src="https://cdn.discordapp.com/attachments/501074912279003166/764433989402361866/7zp9l94277s51.png" alt="Meme"/> 
-			<img src="https://cdn.discordapp.com/attachments/501074912279003166/763712588525469696/lcpvk4gzitr51.png" alt="Meme"/> 
-			<img src="https://cdn.discordapp.com/attachments/501074912279003166/762071120031252500/i3w5wm6h1wq51.png" alt="Meme"/> 
-			<img src="https://preview.redd.it/f3pkrncnbfx51.jpg?width=640&height=640&crop=smart&auto=webp&s=9d0a46add6b807748e70a71b32bbd60a37409f01" alt="Meme"/> 
+			<img src="/memes/meme1.jpg" alt="Meme"/> 
+			<img src="/memes/meme2.jpg" alt="Meme"/> 
+			<img src="/memes/meme3.jpg" alt="Meme"/> 
+			<img src="/memes/meme4.jpg" alt="Meme"/> 
+			<img src="/memes/meme5.jpg" alt="Meme"/> 
+			<img src="/memes/meme6.jpg" alt="Meme"/> 
+			<img src="/memes/meme7.jpg" alt="Meme"/> 
+			<img src="/memes/meme8.jpg" alt="Meme"/> 
+			<img src="/memes/meme9.jpg" alt="Meme"/> 
+			<img src="/memes/meme10.jpg" alt="Meme"/> 
+			<img src="/memes/meme11.jpg" alt="Meme"/> 
+			<img src="/memes/meme34.jpg" alt="Meme"/> 
+			<img src="/memes/meme37.jpg" alt="Meme"/> 
 		</div>
 		<div className="column"> 
-			<img src="https://cdn.discordapp.com/attachments/501074912279003166/723287883670093914/qqp8frr0oo551.png" alt="Meme"/> 
-			<img src="https://cdn.discordapp.com/attachments/501074912279003166/733247114422779982/md9rkw87c5b51.png" alt="Meme"/> 
-			<img src="https://cdn.discordapp.com/attachments/382125465126567936/616220951847370762/t2p7tlrr1tg31.png" alt="Meme"/> 
-			<img src="https://cdn.discordapp.com/attachments/501074912279003166/770416655867183144/3o1kttaxuev51.png" alt="Meme"/> 
-			<img src="https://cdn.discordapp.com/attachments/501074912279003166/768042669674594324/Eksf11vXYAER2KT.png" alt="Meme"/> 
-			<img src="https://cdn.discordapp.com/attachments/501074912279003166/765679007273320478/4p0vgly6m5s51.png" alt="Meme"/> 
-			<img src="https://cdn.discordapp.com/attachments/501074912279003166/765279316963360808/nirp1eac2ns51.png" alt="Meme"/> 
-			<img src="https://cdn.discordapp.com/attachments/501074912279003166/764135753453535242/j3c6ycesj1s51.png" alt="Meme"/> 
-			<img src="https://cdn.discordapp.com/attachments/501074912279003166/763098763598233651/wpbhuo26hhr51.png" alt="Meme"/> 
-			<img src="https://cdn.discordapp.com/attachments/501074912279003166/759758748268101642/xpyl5t3lfjp51.png" alt="Meme"/> 
-			<img src="https://preview.redd.it/2iyydpyoqex51.jpg?width=640&height=405&crop=smart&auto=webp&s=ece7f5c9fba0cd0d3665a593c02c6c477b06f82e" alt="Meme"/> 
+			<img src="/memes/meme12.jpg" alt="Meme"/> 
+			<img src="/memes/meme13.jpg" alt="Meme"/> 
+			<img src="/memes/meme14.jpg" alt="Meme"/> 
+			<img src="/memes/meme15.jpg" alt="Meme"/> 
+			<img src="/memes/meme16.jpg" alt="Meme"/> 
+			<img src="/memes/meme17.jpg" alt="Meme"/> 
+			<img src="/memes/meme18.jpg" alt="Meme"/> 
+			<img src="/memes/meme19.jpg" alt="Meme"/> 
+			<img src="/memes/meme20.jpg" alt="Meme"/> 
+			<img src="/memes/meme21.jpg" alt="Meme"/>  
+			<img src="/memes/meme35.jpg" alt="Meme"/> 
+			<img src="/memes/meme38.jpg" alt="Meme"/> 
 		</div>
 		<div className="column"> 
-			<img src="https://cdn.discordapp.com/attachments/501074912279003166/723972346687258734/fgnv8ttic2651.png" alt="Meme"/> 
-			<img src="https://media.discordapp.net/attachments/501074912279003166/734873999989014659/r_EjANzwO-zLwYrIp1FR14AR-GPnNstsjo_PbfvNovY.png?width=418&height=468" alt="Meme"/> 
-			<img src="https://cdn.discordapp.com/attachments/398492019825311744/686503142275285018/w3wy903nkbz21.png" alt="Meme"/> 
-			<img src="https://cdn.discordapp.com/attachments/501074912279003166/769655833709248512/c8a1a81.jpg" alt="Meme"/> 
-			<img src="https://cdn.discordapp.com/attachments/501074912279003166/767436056450301952/l4tmrfokstt51.png" alt="Meme"/> 
-			<img src="https://cdn.discordapp.com/attachments/501074912279003166/765523741408493568/aabwj3aj1ts51.png" alt="Meme"/> 
-			<img src="https://cdn.discordapp.com/attachments/501074912279003166/765195008646971413/ivvs6565dms51.png" alt="Meme"/> 
-			<img src="https://cdn.discordapp.com/attachments/501074912279003166/763821202660786246/zx2g3ilxvur51.png" alt="Meme"/> 
-			<img src="https://cdn.discordapp.com/attachments/501074912279003166/763098188155846726/eiv7kb99fhr51.png" alt="Meme"/> 
-			<img src="https://cdn.discordapp.com/attachments/501074912279003166/754748597354102794/nl617m7xcum51.png" alt="Meme"/> 
-			
+			<img src="/memes/meme22.jpg" alt="Meme"/>
+			<img src="/memes/meme23.jpg" alt="Meme"/> 
+			<img src="/memes/meme24.jpg" alt="Meme"/> 
+			<img src="/memes/meme25.jpg" alt="Meme"/> 
+			<img src="/memes/meme26.jpg" alt="Meme"/> 
+			<img src="/memes/meme27.jpg" alt="Meme"/> 
+			<img src="/memes/meme28.jpg" alt="Meme"/> 
+			<img src="/memes/meme29.jpg" alt="Meme"/> 
+			<img src="/memes/meme30.jpg" alt="Meme"/> 
+			<img src="/memes/meme31.jpg" alt="Meme"/> 
+			<img src="/memes/meme32.jpg" alt="Meme"/> 
+			<img src="/memes/meme33.jpg" alt="Meme"/> 
+			<img src="/memes/meme36.jpg" alt="Meme"/> 
 		</div>
 		<div className="columnArchives">
 		</div>
