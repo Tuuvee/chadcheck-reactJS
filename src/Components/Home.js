f30f77b07f5cd1f5f5def8070c540d34bbd98133
Tuuvee <tuukka.tahtinen@hotmail.fi>
2021-12-20 12:13:53 +0200

    minor layout fixes

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -12,39 +12,41 @@ function Home(){
 	<div className="row2">
 		<div className="DivList">
 			<h2> HEAD</h2>
-			<ul className="head">
-				<li>BIG</li>
-				<li>FUCKING</li>
-				<li>LARGE</li>
-				<li>-------</li>
-				<li className="alternate">Look at</li>
-				<li className="alternate">that boy</li>
-				<li className="alternate">he lit</li>
-				<li className="alternate">goddamn he</li>
-				<li className="alternate">wilding</li>
-				<li className="alternate">with your</li>
-				<li className="alternate">mami in</li>
-				<li >Miami</li>
+			
+				<p>
+				BIG <br/>
+				FUCKING<br/>
+				LARGE<br/>
+				-------<br/>
+				 Look at<br/>
+				that boy<br/>
+				he lit<br/>
+				goddamn he<br/>
+				wilding<br/>
+				with your<br/>
+				mami in<br/>
+				Miami
+				</p>
 				
-				
-			</ul> 
+			
 		</div>
 		<div className="game">
 				<Game />
 		</div>
 		<div className="DivList">
 			<h2> Uhhh</h2>
-			<ul className="head">
-				<li>Umm</li>
-				<li>Hmmm</li>
-				<li>hummm</li>
-				<li>Basically</li>
-				<li>I uh</li>
-				<li>I dunno</li>
-				<li>What </li>
-				<li>To put </li>
-				<li>here </li>
-			</ul> 
+			<p>
+				Umm<br/>
+				Hmmm<br/>
+				hummm<br/>
+				Basically<br/>
+				<br/>
+				uhhh<br/>
+				Lorem Lipsum<br/>
+				-----------<br/>
+				yeah
+				</p>
+			
 		</div>
 	</div>
 </div>
